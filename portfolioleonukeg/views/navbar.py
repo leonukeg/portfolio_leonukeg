12fa5a0f330e381f121ee351fc86b345e607e066
leonukeg <chemaruan@gmail.com>
2025-06-16 19:18:37 +0200

    Se modifico el navbar

--- portfolioleonukeg/views/navbar.py
+++ portfolioleonukeg/views/navbar.py
@@ -10,13 +10,6 @@ def navbar() -> rx.Component:
             rx.image(src="leonukeg vector_naranja.png", alt="logotipo leonukeg", width= Size.BIG.value, height = Size.BIG.value),
             rx.text("LeonUkeg's Portfolio", ),
             rx.spacer(),
-            rx.hstack(
-                rx.button("About",on_click=rx.redirect("/"), color=Color.PETROL.value, background_color=Color.BASE.value, _hover={"color":Color.ORANGE.value}),
-                rx.button("Projects",on_click=rx.redirect("/"), color=Color.PETROL.value, background_color=Color.BASE.value, _hover={"color":Color.ORANGE.value}),
-                rx.button("Skills",on_click=rx.redirect("/"), color=Color.PETROL.value, background_color=Color.BASE.value, _hover={"color":Color.ORANGE.value}),
-                rx.button("Contact",on_click=rx.redirect("/"), color=Color.PETROL.value, background_color=Color.BASE.value, _hover={"color":Color.ORANGE.value}),
-                
-            ),
             rx.link(rx.image(src="git_hub.png", alt="logo github", width= Size.MEDIUM.value, height = Size.MEDIUM.value), href=constants.GIT_HUB_URL, is_external= True),
             rx.link(rx.image(src="x.png", alt="logo x", width= Size.MEDIUM.value, height = Size.MEDIUM.value),href=constants.X_URL, is_external= True),
             spacing='3',
